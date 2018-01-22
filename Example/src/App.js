762716de665c86b67433320480519a3e44eef522
ting_1_Z <314172670@qq.com>
2018-01-22 17:47:43 +0800

    ant-less-iconfont

---
+++ Example/src/App.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import 'ant-design-pro/dist/ant-design-pro.css'; 
+
+import { Layout, Menu,Icon,Pagination,LocaleProvider ,Table ,Divider} from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+import './App.less';
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+  render: text => <a href="#">{text}</a>,
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}, {
+  title: 'Action',
+  key: 'action',
+  render: (text, record) => (
+    <span>
+      <a href="#">Action 一 {record.name}</a>
+      <Divider type="vertical" />
+      <a href="#">Delete</a>
+      <Divider type="vertical" />
+      <a href="#" className="ant-dropdown-link">
+        More actions <Icon type="down" />
+      </a>
+    </span>
+  ),
+}];
+
+const data = [{
+  key: '1',
+  name: 'John Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  name: 'Jim Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  name: 'Joe Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}];
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+    <Header className="header">
+      <div className="logo"/>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider width={200} style={{ background: '#fff' }}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+          <SubMenu key="sub1" title={<span ><Icon type="all"/>subnav 1</span>}>
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" title={<span><Icon type="back" />subnav 2</span>}>
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" title={<span><Icon type="cart" />subnav 3</span>}>
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+    
+        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+              <LocaleProvider locale={enUS}>
+                <Pagination defaultCurrent={1} total={50} showSizeChanger />
+              </LocaleProvider>
+              <Table columns={columns} dataSource={data}/>
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
+    );
+  }
+}
+
+export default App;
